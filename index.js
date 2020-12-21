cd27ae104802b89dcd7ead8456901b3c3459d3a4
hoang.phanhuy <hoang.phanhuy@vietis.com.vn>
2020-12-21 14:29:56 +0700

    More documentation for index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { getADog, downloadPic } = require('./get-a-dog')
 const PICS_FOLDER = 'dog-pics'
 
 // TODO: #0 - Hove your mouse over getADog to read its documentation
+// The value dog in line 11, 12 represents a dog
+// Try typing `dog.` and see the property suggestions
 getADog((err, dog) => {
   if (err) {
     console.error(err)
@@ -11,9 +13,13 @@ getADog((err, dog) => {
   }
 })
 
-// TODO: #1 - Try downloading the dog's image into `dog-pics` folder
+// TODO: #1 - Get a dog from API
+// Print out its, ID - URL
+// print out the width, height of its image
+
+// TODO: #2 - Try downloading the dog's image into `dog-pics` folder
 // Any dog from getADog has a `url` property, try using that
 // (there is a constant named PICS_FOLDER above)
 // Hint: read about function 'join' of module 'path'
 
-// TODO: #2 - Same task, using promises
+// TODO: #3 - Same task, using promises
